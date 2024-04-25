e24097df24219fa14c09134609f219204128513f
kandanage-1998 <72676622+kandanage-1998@users.noreply.github.com>
2024-04-25 08:29:28 +0530

    add lecturer files
    add  lecturer files (Crud)

---
+++ src/pages/lecturer/addLecturer/index.tsx
@@ -0,0 +1,374 @@
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
+import React, { ChangeEvent, useState } from "react";
+import CustomInput from "../../../components/inputBox";
+import CustomSelect from "../../../components/select";
+import AXIOS_INSTANCE from "../../../services/AxiosInstance";
+import { useDispatch } from "react-redux";
+import { addAlert } from "../../../features/alertSlice";
+import CustomButton from "../../../components/buttons";
+import { useNavigate } from "react-router-dom";
+
+export interface FormData {
+  name: string;
+  nic: string;
+  email: string;
+  address: string;
+  mobile: Number | string;
+  userName: string;
+  password: string | number | undefined;
+  confirmpassword: string | number | undefined;
+}
+
+function AddLecturer() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    nic: "",
+    email: "",
+    address: "",
+    mobile: "",
+    userName: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const lecturerRegisterFunction = async () => {
+    try {
+      const body = {
+        name: formData.name,
+        email: formData.email,
+        address: formData.address,
+        nic: formData.nic,
+        userName: formData.userName,
+        password: formData.password,
+        mobile: formData.mobile,
+      };
+      console.log(body);
+      const data = await AXIOS_INSTANCE.post(`/lecturer`, body);
+      console.log("response", data);
+      if (data.status == 200) {
+        dispatch(
+          addAlert({
+            alertState: true,
+            alertType: "Success",
+            alertMessage: "Successfully Added",
+            alertDescription: "Lecturer Registration is successfully completed.",
+          })
+        );
+      }
+      navigate("/lecturer/lecturer")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Box
+      sx={{
+        padding: "30px",
+        flex: 1,
+        overflow: "hidden",
+        overflowY: "auto",
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography
+          sx={{
+            fontSize: "24px",
+            marginBottom: "20px",
+            textAlign: "center",
+            backgroundColor: "#024D81",
+            color: "white",
+            borderRadius: "10px",
+            height: "40px",
+          }}
+        >
+          Lecturer Register
+        </Typography>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>Name</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                style={{
+                  width: "575px",
+                }}
+                id={0}
+                TextFieldName={"name"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.name}
+                textFieldSize={"small"}
+                placeHolderText={"Name"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>NIC</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                style={{
+                  width: "575px",
+                }}
+                id={0}
+                TextFieldName={"nic"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.nic}
+                textFieldSize={"small"}
+                placeHolderText={"NIC"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>Email</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                style={{
+                  width: "575px",
+                }}
+                id={0}
+                TextFieldName={"email"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.email}
+                textFieldSize={"small"}
+                placeHolderText={"Email"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>Address</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                id={0}
+                style={{
+                  width: "575px",
+                }}
+                TextFieldName={"address"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.address}
+                textFieldSize={"small"}
+                placeHolderText={"Address"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>Mobile</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                id={0}
+                style={{
+                  width: "575px",
+                }}
+                TextFieldName={"mobile"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.mobile}
+                textFieldSize={"small"}
+                placeHolderText={"Mobile"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={2}
+        justifyContent="flex-start"
+        alignItems="center"
+        sx={{
+          padding: 0,
+          backgroundColor: "#ffffff",
+          marginTop: "20px",
+          borderTop: "1px solid #ccc",
+        }}
+      >
+        <Grid item xs={12}>
+          <Typography
+            sx={{
+              color: "#ACACAC",
+            }}
+          >
+            Add Credentials
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>User Name</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                id={0}
+                style={{
+                  width: "575px",
+                }}
+                TextFieldName={"userName"}
+                labelText={""}
+                TextFieldType={"text"}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.userName}
+                textFieldSize={"small"}
+                placeHolderText={"User Name"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>New Password</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                id={3}
+                style={{
+                  width: "575px",
+                }}
+                TextFieldName={"password"}
+                labelText={""}
+                placeHolderText={"New Password"}
+                TextFieldType={""}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.password}
+                textFieldSize={"small"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography>Confirm Password</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomInput
+                id={3}
+                style={{
+                  width: "575px",
+                }}
+                TextFieldName={"confirmpassword"}
+                labelText={""}
+                placeHolderText={"Confirm Password"}
+                TextFieldType={""}
+                variant={"outlined"}
+                onchangeFunction={handleFieldChange}
+                errorTextState={false}
+                errorText={""}
+                value={formData.confirmpassword}
+                textFieldSize={"small"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <div style={{ marginTop: "auto" }}>
+        <Grid
+          container
+          justifyContent="flex-end"
+          alignItems="center"
+          sx={{
+            padding: 2,
+            backgroundColor: "#ffffff",
+            gap: "8px",
+            marginTop: "auto",
+            borderTop: "1px solid #ccc",
+          }}
+        >
+          <Grid item>
+            <CustomButton
+              variant={"outlined"}
+              buttonText={"Cancel"}
+              id={1}
+              buttonFunction={() => {
+                //   handleClosePopup()
+              }}
+              style={{
+                borderRadius: "10px",
+                width: "110px",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <CustomButton
+              variant={"contained"}
+              buttonText={
+                loading ? (
+                  <CircularProgress sx={{ color: "white" }} size={28} />
+                ) : (
+                  "Create"
+                )
+              }
+              disableState={loading ? true : false}
+              id={0}
+              buttonFunction={() => {
+                lecturerRegisterFunction();
+              }}
+              style={{
+                borderRadius: "10px",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </Box>
+  );
+}
+
+export default AddLecturer;
--- src/pages/lecturer/index.tsx
+++ src/pages/lecturer/index.tsx
@@ -1,10 +1,121 @@
-import React from 'react'
+import { Box, Paper, Typography } from '@mui/material'
+import { GridColDef } from '@mui/x-data-grid'
+import React, { useEffect, useState } from 'react'
+import AXIOS_INSTANCE from '../../services/AxiosInstance'
+import CustomTable from '../../components/dataGrid'
+import LecturerView from './LecturerView'
+
 
 function Lecturer() {
+  const [tablePage, setTablePage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [tableRow, setTableRow] = useState([])
+  const [resetFilter, setResetFilter] = useState(false)
+  const [selectedRows, setSelectedRows] = useState([])
+  const [tableAllRowCount, setTableAllRowCount] = useState(0)
+  const [selectedLecturerID, setSelectedLecturerID] = useState<number>()
+  const [isDetailsPopupOpen, setIsDetailsPopupOpen] = useState<boolean>(false)
+  const handleOpenDetailPopup = () => {
+    setIsDetailsPopupOpen(true)
+  }
+
+  const handleDetailClosePopup = () => {
+    fetchRowData(false, false)
+    setIsDetailsPopupOpen(false)
+  }
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      align: "left",
+      renderCell: (params) => {
+        return (
+          <Typography
+            sx={{
+              fontSize: "14px",
+              textDecoration: "underline",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              setSelectedLecturerID(params?.row?.id)
+              handleOpenDetailPopup()
+            }}>
+            {params.value}
+          </Typography>
+        )
+      },
+    },
+    { field: "email", headerName: "Email Address", flex: 1, align: "left" },
+    { field: "mobile", headerName: "Contact Number", flex: 1, align: "left" },
+    { field: "userName", headerName: "User Name", flex: 1, align: "left" },
+    { field: "address", headerName: "Address", flex: 1, align: "left" },
+    { field: "nic", headerName: "NIC", flex: 1, align: "left" },
+  ]
+  const fetchRowData = async (filterState: boolean, exportSate: boolean) => {
+    let parameters: any = {
+      page: tablePage,
+    }
+    
+    try {
+      setLoading(true)
+      const { data } = await AXIOS_INSTANCE.get(`/lecturer`, {
+        // params: parameters,
+      })
+      console.log(data)
+      setTableRow(data)
+      setTableAllRowCount(data.admin.total)
+      setResetFilter(false)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => {
+    fetchRowData(false, false)
+  }, [tablePage])
   return (
-    <div>
-      Lecturer
+    <>
+    <Box sx={{ width: "100%", height: "100%" }}>
+    <Paper
+          sx={{
+            padding: 2,
+            display: "flex",
+            gap: 2,
+            flexDirection: "column",
+            width: "100%",
+            height: "100%",
+            border: "1px solid #EAECF0",
+            borderRadius: 5,
+            zIndex: 5,
+          }}>
+    <div style={{ width: "100%", height: "100%" }}>
+      <CustomTable
+        rows={tableRow}
+        columns={columns}
+        checkboxState={true}
+        columnGroupingState={false}
+        columnGroupingModel={[]}
+        // allRowCount={tableAllRowCount}
+        pageSize={[10, 20]}
+        // page={tablePage - 1}
+        // dataLoading={loading}
+        // onPageChange={handlePageChange}
+        setSelectedRows={setSelectedRows}
+      />
     </div>
+    </Paper>
+    {isDetailsPopupOpen && (
+        <LecturerView
+          isPopupOpen={isDetailsPopupOpen}
+          handleClosePopup={handleDetailClosePopup}
+          selectedStudentID={selectedLecturerID}
+        />
+      )}
+    </Box>
+    </>
   )
 }
 
