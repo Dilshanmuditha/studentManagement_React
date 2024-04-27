76573f6b06252f1550abb2ac9e535c42e14e4dfe
kandanage-1998 <72676622+kandanage-1998@users.noreply.github.com>
2024-04-27 12:47:45 +0530

    add some changes
    add some changes

--- src/pages/module/index.tsx
+++ src/pages/module/index.tsx
@@ -17,23 +17,26 @@ function CourseList() {
   const [loading, setLoading] = useState<boolean>(false);
   const [courses, setCourses] = useState<any[]>([]);
   const [expandedModule, setExpandedModule] = useState<string>('');
+  
   const fetchCourseData = async () => {
     try {
-      const response = await AXIOS_INSTANCE.get(/course)
-      console.log(response)
-        setCourses(response?.data)
+      const response = await AXIOS_INSTANCE.get('/course');
+      console.log(response);
+      setCourses(response?.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  const Course = async () => {
+  };
+
+  const registerCourse = async (id: string) => {
     try {
       const body = {
-        id: '0',
+        id: id,
         name: '',
       };
       console.log(body);
-      const data = await AXIOS_INSTANCE.post(/courses, body);
+      const data = await AXIOS_INSTANCE.post('/courses', body);
+
       console.log("response", data);
       if (data.status === 200) {
         dispatch(
@@ -52,8 +55,8 @@ function CourseList() {
   };
 
   useEffect(() => {
-    fetchCourseData()
-  }, [])
+    fetchCourseData();
+  }, []);
 
   return (
     <Box
@@ -82,8 +85,7 @@ function CourseList() {
       <Grid container spacing={2}>
         {courses.map((course) => (
           <Grid item xs={4} key={course.id}>
-            <Link to={/course/${course.id}} style={{ textDecoration: "none" }}>
-            
+            <Link to={`/course/${course.id}`} style={{ textDecoration: "none" }}>
               <Typography
                 sx={{
                   backgroundColor: "#024D81",
@@ -95,11 +97,11 @@ function CourseList() {
                   alignItems: "center",
                 }}
               >
-                 course code : {course?.code}
-                  <br/>
-                course name : {course.name}
+                Course code: {course?.code}
+                <br />
+                Course name: {course.name}
               </Typography>
-              <button onClick={() => Course()} style={{ width: '100%', marginTop: '10px' }}>
+              <button onClick={() => registerCourse(course.id)} style={{ width: '100%', marginTop: '10px' }}>
                 Click Here To Course Register
               </button>
             </Link>
@@ -110,4 +112,4 @@ function CourseList() {
   );
 }
 
-export default CourseList;
+export default CourseList;
